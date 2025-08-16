90e4e4ab86474f46865ed01aacb4bb571837b36b
Ali Qambari <usend@live.com>
2025-08-16 22:10:50 +0330

    Fix(models): get rid of invalid role assignment

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .enums import UserRole
+from django.core.exceptions import ValidationError
 
 class User(AbstractUser):
     role = models.CharField(
@@ -11,8 +12,15 @@ class User(AbstractUser):
     version = models.PositiveIntegerField(default=1) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def clean(self):
+        # Ensure role is one of the allowed choices (shell problem)
+        if self.role not in UserRole.values:
+            raise ValidationError({"role": f"Invalid role '{self.role}'. Must be one of {list(UserRole.values)}."})
 
+    
     def save(self, *args, **kwargs):
+        self.full_clean()   #run clean()
         # Auto-increment version on update
         if self.pk:
             self.version += 1
